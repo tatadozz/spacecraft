74224e9209814903d4358fd83fabe1be2bce43af
Clément LEFIEF <clement.lefief@gmail.com>
2022-02-01 08:56:06 +0100

    next step

---
+++ src/components/Loader.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import ContentLoader, { Rect, Circle, Path } from "react-content-loader/native"
+
+const MyLoader = (props) => (
+    <ContentLoader
+        speed={2}
+        width={540}
+        height={800}
+        viewBox="0 0 400 160"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+        {...props}
+    >
+        <Rect x="10" y="30" rx="0" ry="0" width="300" height="300" />
+    </ContentLoader>
+)
+
+export default MyLoader
+
+
--- src/screens/FeedScreen.tsx
+++ src/screens/FeedScreen.tsx
@@ -1,26 +1,20 @@
 import React from "react";
-import ContentLoader from "react-content-loader";
 import { SafeAreaView, StyleSheet, Text, StatusBar, View, FlatList, SafeAreaViewBase } from "react-native";
 import { Button, Colors } from "react-native-paper";
-import { Circle, Rect } from "react-native-svg";
-
 import { useStarships } from "../../hooks/useStarships";
 import CardItem from "../components/CardItem";
 import Header from "../components/Header";
 import HeadTitle from "../components/HeadTitle";
 import ContentLoader, { Rect, Circle } from 'react-content-loader/native'
+import MyLoader from "../components/Loader";
 
-const App = () => {
+const FeedScreen = () => {
     const { isLoading, isError, data, refetch } = useStarships();
     if (isLoading) {
         return (
             <SafeAreaView>
                 <HeadTitle title={"⏳ Loading..."} />
-                <ContentLoader viewBox="0 0 380 70">
-                    <Circle cx="30" cy="30" r="30" />
-                    <Rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
-                    <Rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
-                </ContentLoader>
+                <MyLoader />
             </SafeAreaView >
         )
     }
@@ -65,4 +59,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default App;
+export default FeedScreen;
